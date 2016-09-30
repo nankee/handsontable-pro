0a1d56c7c6f31e2d1d65d01b5865fe586b9f2c12
Jan Siegel <js.ziggle@gmail.com>
2016-09-30 08:46:03 +0200

    Add bower_components to gitignore, remove the bower_components directory from the repo.

--- bower_components/hot-formula-parser/test/unit/helper/string.js
+++
@@ -1,9 +0,0 @@
-import {trimEdges} from '../../../src/helper/string';
-
-describe('.trimEdges()', () => {
-  it('should correctly trim edges', () => {
-    expect(trimEdges('hello')).to.eq('ell');
-    expect(trimEdges('hello', 1)).to.eq('ell');
-    expect(trimEdges('hello', 2)).to.eq('l');
-  });
-});
